86366a48f71c72af441f2434b007e7755934751a
emt84 <79057713+emt84@users.noreply.github.com>
2023-11-24 14:55:20 -0500

    Stiffness control/curve and Whole control

--- motor control/stepper.py
+++ motor control/stepper.py
@@ -114,3 +114,49 @@ class claw:
     steps = round(self.gear_ratio*angle/1.8)
     #move the motor to the calculated steps
     stepper.set_loc(steps)
+
+class stiffness:
+  def __init__(self, stepper):
+    self.stepper = stepper  #STEPPER - the stepper object corresponding to the stiffness control
+
+  #calculate the location for *stiff(float)*, stiffness should be in N/mm
+  def calc_loc(stiff):
+    #fitted equation from empirical data
+    def eqn(loc):
+      return .0001769*loc**2+.0022213*loc+.1423-stiff
+    #calculate location
+    sol = root_scalar(eqn, x0=20, method='secant')
+    loc = round(sol.root,5)
+    return loc
+
+  #sets stiffness actuator to given *stiff(float)*, stiffness should be in N/mm
+  def set_stiffness(stiff):
+    loc = calc_loc(stiff)
+    self.stepper.set_loc(loc)
+
+class whole:
+  def __init__(self,claw,stiff1,stiff2):
+    self.claw = claw #CLAW - claw object
+    self.stiff1 = stiff1 #STIFFNESS - stiffness object 
+    self.stiff2 = stiff2 #STIFFNESS - stiffness object
+
+  #set width and stiffness for object with given *width(float)*, *weight(float)*, and *strength(float)*
+  #width should be in mm
+  #weight should be in grams
+  #strength varies from 0-1 with 0 being very fragile and 1 being relatively sturdy
+  def grab_obj(width, weight, strength):
+    #calc force required to pick up object, .1 assumed for coeffecient of friction
+    # (sheets typically bend to vertical position so little normal force)
+    force = weight/1000 * 9.81 / .1 / 2
+
+    #linear relationship between strength and stifness
+    stiffness = .15+strength*.6
+    #calculate displacement based on calculated force and stiffness
+    disp = force/stiffness
+    #estimate width based on width and strength
+    width = width-disp*2-strength*width*.25
+
+    #set all parts to respective setttingsß
+    self.claw.set_width(width)
+    self.stiff1.set_stiffness(stiffness)
+    self.stiff2.set_stiffness(stiffness)
