ecaf076c89a04e6486799360bcb55edda3cb8271
emt84 <79057713+emt84@users.noreply.github.com>
2023-11-29 15:37:07 -0500

    Bugs and Uncomment Pi stuff

--- motor control/example.py
+++ motor control/example.py
@@ -1,14 +1,14 @@
 from stepper import *
 
 #initialize objects
-m1 = stepper(120,"cw",2,3,4,11,False)
+m1 = stepper(120,"ccw",2,3,4,17,False)
 m2 = stepper(120,"cw",14,15,18,23,True)
-m3 = stepper(120,"cw",22,10,9,11,False)
+m3 = stepper(120,"ccw",22,10,9,11,False)
 
 #zero each motor
-# m1.zero()
-# m2.zero()
-# m3.zero()
+m1.zero()
+m2.zero()
+m3.zero()
 
 #set each motor to a different location
 # m1.set_loc(20)
@@ -18,18 +18,18 @@ m3 = stepper(120,"cw",22,10,9,11,False)
 #use claw to control motor 2
 c = claw(m2,100,100,4,6,3,20)
 #set claw so tips of sheets are touching
-c.set_width(0)
+#c.set_width(0)
 
 
 #2 stiffness objects
 s1 = stiffness(m1)
-s2 = stiffness(m2)
+s2 = stiffness(m3)
 
 #create whole object
 w = whole(c,s1,s2)
 
-#pick up 30 gram, 30mm wide object with .5 strength
-w.grab_obj(30,40,.5)
-
 #open to max size
 #w.open_up()
+
+#pick up 30 gram, 30mm wide object with .5 strength
+w.grab_obj(20,50,1)
